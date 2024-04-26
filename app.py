470ee5e1a01468e0922efd76a1426a37b7ea232b
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-26 10:35:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,28 +7,11 @@ st.markdown('# Guess Who? - Pokemon Edition')
 
 idx = random.sample(range(1, 1026), 25)
 
-'''
-for col in row1 + row2 + row3 + row4 + row5 + row6:
-    tile = col.container(height=100)
-    for i in idx:
-        url = "https://github.com/PokeAPI/sprites/blob/ca5a7886c10753144e6fae3b69d45a4d42a449b4/sprites/pokemon/{id}.png?raw=true"
-        pokemon_id = str(i)
-        response = requests.get(url.format(id=pokemon_id))
-    tile.image(response.content)
-for i in idx:
-    url = "https://github.com/PokeAPI/sprites/blob/ca5a7886c10753144e6fae3b69d45a4d42a449b4/sprites/pokemon/{id}.png?raw=true"
-    pokemon_id = str(i)
-    response = requests.get(url.format(id=pokemon_id))
-    for col in row1 + row2 + row3 + row4 + row5:
-        tile = col.container(border=True)
-        tile.image(response.content)
-'''
 n_cols = 5
 data = []
 for i in idx:
   url = "https://github.com/PokeAPI/sprites/blob/ca5a7886c10753144e6fae3b69d45a4d42a449b4/sprites/pokemon/{id}.png?raw=true"
   pokemon_id = str(i)
-
   response = requests.get(url.format(id=pokemon_id))
   data.append(response.content)
 
