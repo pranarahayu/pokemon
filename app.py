327a23f828c85e1e907b26184214bf2ac9fce900
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-26 14:24:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ with st.sidebar:
       nama = responsex.json()
       for item in nama['forms']:
         name.append(item['name'].title())
-    conn.table("temp_pokemon").insert([{"id":idx,"pokemon":name}], count="None").execute()
+    for j in enumerate(idx):
+      conn.table("temp_pokemon").insert([{"id":idx[j],"pokemon":name[j]}], count="None").execute()
   with st.expander("About this app"):
     st.markdown("Guess who, but Pokemon.")
     
