af97fd718d03177bfb1cd31ddb1cf773c1c735c8
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-26 09:24:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ import streamlit as st
 import requests
 import random
 
+st.set_page_config(page_title='Match Center', layout='wide')
+st.markdown('# Guess Who? - Pokemon Edition')
+
+@st.cache_data(ttl=600)
+idx = random.sample(range(1, 1026), 30)
+
 row1 = st.columns(5)
 row2 = st.columns(5)
 row3 = st.columns(5)
@@ -11,3 +17,8 @@ row6 = st.columns(5)
 
 for col in row1 + row2 + row3 + row4 + row5 + row6:
     tile = col.container(height=100)
+    for i in idx:
+        url = "https://github.com/PokeAPI/sprites/blob/ca5a7886c10753144e6fae3b69d45a4d42a449b4/sprites/pokemon/{id}.png?raw=true"
+        pokemon_id = str(i)
+        response = requests.get(url.format(id=pokemon_id))
+        tile.image(response.content)
