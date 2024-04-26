1319166e26fe1441f9d060c6b094060570b5bf50
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-26 09:25:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ row5 = st.columns(5)
 row6 = st.columns(5)
 
 for col in row1 + row2 + row3 + row4 + row5 + row6:
-    tile = col.container(height=100)
     for i in idx:
         url = "https://github.com/PokeAPI/sprites/blob/ca5a7886c10753144e6fae3b69d45a4d42a449b4/sprites/pokemon/{id}.png?raw=true"
         pokemon_id = str(i)
         response = requests.get(url.format(id=pokemon_id))
+        tile = col.container(height=100)
         tile.image(response.content)
