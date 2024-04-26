75983cf40d7ad13c199e0d5fa56bfcfc07d71d66
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-04-26 10:34:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ for col in row1 + row2 + row3 + row4 + row5 + row6:
         pokemon_id = str(i)
         response = requests.get(url.format(id=pokemon_id))
     tile.image(response.content)
-'''
 for i in idx:
     url = "https://github.com/PokeAPI/sprites/blob/ca5a7886c10753144e6fae3b69d45a4d42a449b4/sprites/pokemon/{id}.png?raw=true"
     pokemon_id = str(i)
@@ -29,3 +28,20 @@ for i in idx:
     for col in row1 + row2 + row3 + row4 + row5:
         tile = col.container(border=True)
         tile.image(response.content)
+'''
+n_cols = 5
+data = []
+for i in idx:
+  url = "https://github.com/PokeAPI/sprites/blob/ca5a7886c10753144e6fae3b69d45a4d42a449b4/sprites/pokemon/{id}.png?raw=true"
+  pokemon_id = str(i)
+
+  response = requests.get(url.format(id=pokemon_id))
+  data.append(response.content)
+
+n_rows = 1 + len(data) // int(n_cols)
+rows = [st.container() for _ in range(n_rows)]
+cols_per_row = [r.columns(n_cols) for r in rows]
+cols = [column for row in cols_per_row for column in row]
+
+for image_index, data in enumerate(data):
+    cols[image_index].image(data)
